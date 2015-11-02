d4f1b88fa420fb84c504fe5e7f8e6a98e39b8588
shawnspears <shwnspears@gmail.com>
2015-11-01 18:52:54 -0800

    Add reflection in solution file

--- week-5/calculate-mode/my_solution.rb
+++ week-5/calculate-mode/my_solution.rb
@@ -46,7 +46,7 @@ def mode(array)
   counter = Hash.new(0)
   array.each do |i|
     counter[i] += 1
-end  
+  end  
   mode = []
   counter.each do |key, value|
     if value == counter.values.max
@@ -54,7 +54,37 @@ end
     end
   end
   p mode
+  if mode == array
+    p "All occur with equal frequency"
+  end
 end
 
 
 # 4. Reflection
+
+# Which data structure did you and your pair decide to implement and why?
+#   My partner and I decided to implement a hash because we figured it would be the most intuitive
+#   data structure in order to indicate the number of times that a particular number comes up in an
+#   array. In our code we utilized Hash.new(0) as a counter that incremented +1 each time it iterated
+#   over the same key, or counter[i]. We were then able to take the counter hash and select the max
+#   value, which also happens to be the mode. In order to return the output as an array, we created
+#   an array with the keys associated with the max value of the couter hash and returned that.
+
+# Were you more successful breaking this problem down into implementable pseudocode than the last with a pair? 
+#   I definitely think that each time I pseudocode it becomes slightly easier to be able to write out
+#   a solution that is more easily translatable into code because I know what is more viable at this point.
+#   We were able to psuedocode something that I thought was clear and succinct. Our initial solution
+#   is really reflective of that, as it follows our steps fairly closely.
+
+# What issues/successes did you run into when translating your pseudocode to code?
+#   As I mentioned preiously, we didn't really run into many problems while translating our pseudocode
+#   into our initial solution because we talked through a possible solution while keeping a result in
+#   mind. I think running through a refactor was more difficult because we wanted the output to be an
+#   array.
+
+# What methods did you use to iterate through the content? Did you find any good ones when you were refactoring? Were they difficult to implement?
+#   We used the array.each method in order to iterate through the array and increment the values to counter
+#   for each index (key). We then used the hash.each method in order to iterate through each of the values 
+#   and find the max value, and then return its associate key (or the mode). Unfortunately we weren't able
+#   to find any other iterative methods that did something similar -- which I'm sure exist. I'll look
+#   through the docs in the near future and see if there is something more efficient to implement.
