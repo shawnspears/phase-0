7821e1efa80e45018ee5591f52cb211e367486ad
shawnspears <shwnspears@gmail.com>
2015-11-09 13:31:01 -0800

    Add group-project dir and associated files

---
+++ week-7/group-project/group_project_solution.js
@@ -0,0 +1,48 @@
+
+// Release 1: Tests to User Stories (Shawn Spears)
+
+// ------ User Stories ------
+
+// As a user, I want to be able to be able to take two ordered lists of integers, and find the
+// sum, mean, and median of each (Three different stories).
+
+// The first list contains 7 integers: 1, 2, 3, 4, 5, 5, and 7. Therefore the length of this list is 7,
+// which is odd. If I had to name it, I would call it the odd length list.
+
+// The second list contains 8 integers: 4, 4, 5, 5, 6, 6, 6, and 7. Therefore the length of this list
+// is 8, which is even. If I had to name it, I would call it the even length list.
+
+// I want to find the sum, mean, and median of each of these lists separately, so separate functions
+// would probably be the best way for me to get the results I want:
+
+// ------ Story 1 ------
+
+// In the sum function, if I inputted either the odd length or even length list of integers, it should 
+// return in the sum of those integers. 
+
+// ------ Story 2 ------
+
+// In the mean function, if I inputted either the odd length or even length list of integers, it should
+// return the average of those integers as a decimal point number.
+
+// ------ Story 3 ------
+
+// In the median function, if I inputted either the odd length or even length list of integers, it should
+// return the median of those integers as a decimal point number. 
+
+// (If you've forgotten what the median is, it is simply the middle value in a list of values. For an odd
+// numbered list of values, there is an obvious middle as the two other halves apart from the middle value 
+// can be separated equally. In an even list, there will be two middle values -- the median will be the 
+// average of these values.)
+
+
+
+
+
+
+
+
+
+
+
+
