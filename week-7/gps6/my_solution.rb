e51b7ea9f7d48ca96cf564d3e513518ac0118f41
shawnspears <shwnspears@gmail.com>
2015-11-15 15:47:07 -0800

    Edit solution in my_solution.rb

--- week-7/gps6/my_solution.rb
+++ week-7/gps6/my_solution.rb
@@ -1,7 +1,7 @@
 # Virus Predictor
 
-# I worked on this challenge [by myself, with: ].
-# We spent [#] hours on this challenge.
+# I worked on this challenge [with: Josh Shin].
+# We spent [1] hours on this challenge.
 
 # EXPLANATION OF require_relative
 #
@@ -17,49 +17,45 @@ class VirusPredictor
     @population_density = population_density
   end
   
+
   # Predicts effects of virus based on methods predicted_deaths and speed_of_spread
   def virus_effects
-    predicted_deaths(@population_density, @population, @state)
-    speed_of_spread(@population_density, @state)
+    predicted_deaths
+    speed_of_spread
   end
 
+
   private
-
   # Predicts deaths based on population density and population of state. The greater the population density, the greater the ratio of the population that will die.
-  def predicted_deaths(population_density, population, state)
+  def predicted_deaths
     # predicted deaths is solely based on population density
-    if @population_density >= 200
-      number_of_deaths = (@population * 0.4).floor
-    elsif @population_density >= 150
-      number_of_deaths = (@population * 0.3).floor
-    elsif @population_density >= 100
-      number_of_deaths = (@population * 0.2).floor
-    elsif @population_density >= 50
-      number_of_deaths = (@population * 0.1).floor
-    else
+    case @population_density
+    when 0..49
       number_of_deaths = (@population * 0.05).floor
+    when 50..99
+      number_of_deaths = (@population * 0.1).floor
+    when 100..149
+      number_of_deaths = (@population * 0.2).floor
+    when 150..199
+      number_of_deaths = (@population * 0.3).floor
+    else
+     number_of_deaths = (@population * 0.4).floor
     end
-
+    
     print "#{@state} will lose #{number_of_deaths} people in this outbreak"
 
   end
 
   # Predicts the spead of spread based on the population density of the state
-  def speed_of_spread(population_density, state) #in months
+  def speed_of_spread #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
-    speed = 0.0
-
-    if @population_density >= 200
-      speed += 0.5
-    elsif @population_density >= 150
-      speed += 1
-    elsif @population_density >= 100
-      speed += 1.5
-    elsif @population_density >= 50
-      speed += 2
-    else
-      speed += 2.5
+    case @population_density
+    when 0..49 then speed = 2.5
+    when 50..99 then speed = 2
+    when 100..149 then speed = 1.5
+    when 150..199 then speed = 1
+    else speed = 0.5 
     end
 
     puts " and will spread across the state in #{speed} months.\n\n"
@@ -96,3 +92,9 @@ end
 
 #=======================================================================
 # Reflection Section
+
+# What are the differences between the two different hash syntaxes shown in the state_data file?
+# What does require_relative do? How is it different from require?
+# What are some ways to iterate through a hash?
+# When refactoring virus_effects, what stood out to you about the variables, if anything?
+# What concept did you most solidify in this challenge?
